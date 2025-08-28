82c6ae3ef116a77039bf1b871a7b2765c3fec1eb
ndlevinzon <ndlevinzon@gmail.com>
2025-08-28 13:18:07 -0600

    making the cli interactive

--- src/__init__.py
+++ src/__init__.py
@@ -7,4 +7,4 @@ from .config_manager import ConfigManager
 __version__ = "0.1.0"
 __author__ = "Nathan Levinzon & Shen Lab Team @ The University of Utah"
 
-__all__ = ["ConfigManager"]
+__all__ = ["ConfigManager", "CryoDLShell"]
