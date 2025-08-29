bee9e18c4f3e46c59819b65adb4804fb862c158d
ndlevinzon <ndlevinzon@gmail.com>
2025-08-29 13:57:16 -0600

    Adding documentation via Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
     'sphinx_rtd_theme',
     'sphinx_copybutton',
     'sphinx.ext.autosummary',
+    'myst_parser',  # Enable MyST parser for Markdown support
 ]
 
 # Add any paths that contain templates here, relative to this directory.
