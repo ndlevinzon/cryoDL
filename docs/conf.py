f1407a16745c73c4dc2a4854775977d1cd237122
ndlevinzon <ndlevinzon@gmail.com>
2025-08-29 15:09:38 -0600

    Fixes example parsing in napoleon for google doc strings

--- docs/conf.py
+++ docs/conf.py
@@ -168,18 +168,22 @@ autodoc_default_options = {
 }
 
 # Napoleon settings for Google-style docstrings
+# Napoleon settings for Google-style docstrings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = True
 napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = True
 napoleon_use_ivar = True
 napoleon_use_param = True
 napoleon_use_rtype = True
 napoleon_use_keyword = True
+napoleon_preprocess_types = True
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
 
 # Intersphinx mapping
 intersphinx_mapping = {
