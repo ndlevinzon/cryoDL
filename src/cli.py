02dc1a4aa78bf09e26bebd02a53282b1687bf904
ndlevinzon <ndlevinzon@gmail.com>
2025-08-31 13:28:16 -0600

    Added fasta annotations and updated documentation

--- src/cli.py
+++ src/cli.py
@@ -734,7 +734,7 @@ All interactions are logged to cryodl.log in the current directory.
             fasta 1ABC
             fasta 1ABC --output my_protein.fasta
             fasta --multiple 1ABC 2DEF 3GHI --output combined.fasta
-            fasta --annotate model.cif protein.fasta --output annotated.fasta
+            fasta --annotate model.cif protein.fasta --output annotations.txt
         """
         self.log_command("fasta", arg)
         try:
@@ -776,7 +776,7 @@ All interactions are logged to cryodl.log in the current directory.
 
                 cif_file = args[1]
                 fasta_file = args[2]
-                output_file = "annotated_sequence.fasta"
+                output_file = "annotations.txt"
 
                 # Check for output file option
                 if len(args) >= 5 and args[3] == "--output":
